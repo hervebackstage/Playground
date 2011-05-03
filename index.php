3f0ccf5402eaa5cfbe48556bcf119b32d8c699a4
Herve Tribouilloy <herve.tribouilloy@willoughbystewart.com>
2011-05-03 12:15:30 +0100

    Add fotter and renamed logo image

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 echo 'Hello world';
 ?>
-This is some text to describe the first page of the site. Welcome to the site PLayground. Its aim is to play with Git to learn to commit with this repository tool.:w
+This is some text to describe the first page of the site. Welcome to the site PLayground. Its aim is to play with Git to learn to commit with this repository tool.
 
-<img src="logo.jpg" />
+<img src="branded-keeper-logo.gif" />
 
-
+This is the footer of the site
 

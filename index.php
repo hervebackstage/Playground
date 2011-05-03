d8cf461b84c962e93711836fa38b37042e333654
Herve Tribouilloy <herve.tribouilloy@willoughbystewart.com>
2011-05-03 12:13:18 +0100

    Add logo image to site

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ echo 'Hello world';
 ?>
 This is some text to describe the first page of the site. Welcome to the site PLayground. Its aim is to play with Git to learn to commit with this repository tool.:w
 
+<img src="logo.jpg" />
 
 
 

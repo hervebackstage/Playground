b6bdcaaac79d1c6565654b897afed6687f1d8ea3
Herve Tribouilloy <herve.tribouilloy@willoughbystewart.com>
2011-05-03 12:05:50 +0100

    Welcome text added on the sitew

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 echo 'Hello world';
 ?>
+This is some text to describe the first page of the site. Welcome to the site PLayground. Its aim is to play with Git to learn to commit with this repository tool.:w
+
 
 
 
